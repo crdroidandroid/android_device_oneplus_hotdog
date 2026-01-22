1a7e5dc62a2f9f77c1d1aeb06051eed8222f6416
ShevT <evgeny.shishkov@gmail.com>
2026-01-22 14:37:32 +0300

    hotdog: overlay: Drop KeyHandlerResTarget
    Reason: We use OPlusExtras
    
    Change-Id: Ib1208974aee88e3dcd18635314fe40b3ba9fcdef

--- device.mk
+++ device.mk
@@ -32,7 +32,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    KeyHandlerResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
